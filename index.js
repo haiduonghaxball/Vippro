e95895a1ed0792a893b3658eb925b5d6114061a0
haiduonghaxball <haiduong20210@gmail.com>
2025-07-10 01:16:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,23 +11,16 @@ const fs = require('fs');
   const page = await browser.newPage();
   await page.goto('https://www.haxball.com/headless', { waitUntil: 'networkidle2' });
 
-  // Chờ Haxball load xong HBInit
+  // Chờ Haxball load HBInit
   await page.waitForFunction(() => typeof HBInit !== 'undefined');
   console.log("✅ Haxball Headless đã load.");
 
-  // Load script RS.js
+  // Load script RS.js + ép return room link
   const script = fs.readFileSync('./RS.js', 'utf8');
-  await page.evaluate(script => {
+  const roomLink = await page.evaluate(script => {
     eval(script);
+    return room.roomLink;
   }, script);
-  console.log("✅ Đã inject RS.js vào room.");
 
-  // Chờ room được tạo xong
-  await page.waitForFunction(() => typeof room !== 'undefined' && room.roomLink !== undefined);
-  console.log("✅ Room đã được tạo.");
-
-  // Lấy link room
-  const roomLinkHandle = await page.evaluateHandle(() => room.roomLink);
-  const roomLink = await roomLinkHandle.jsonValue();
   console.log("🎯 ROOM LINK:", roomLink);
 })();
